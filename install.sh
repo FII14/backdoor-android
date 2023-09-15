0759421462b2ae0d697e0bbc1cbe2bfe67401ec7
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-16 01:22:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-apt-get install apktool
-apt-get install zipalign
-apt-get install default-jdk
+apt-get install wget && apt-get install apktool && apt-get install jarsigner && apt-get install openjdk-11-jdk
+wget "https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool"
+wget "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar"
+cp "apktool" "/usr/local/bin"
+mv "apktool_2.8.1.jar" "apktool.jar"
+cp "apktool.jar" "/usr/local/bin"
